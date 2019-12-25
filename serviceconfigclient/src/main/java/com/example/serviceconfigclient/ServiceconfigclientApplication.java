e8efe9c63f2147374c0b2086f18e30875372bb9c
Hxy <houxuyang0801@163.com>
2019-12-25 14:58:55 +0800

    添加serviceconfigclient模块

---
+++ serviceconfigclient/src/main/java/com/example/serviceconfigclient/ServiceconfigclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceconfigclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceconfigclientApplication.class, args);
+    }
+
+}
---
+++ serviceconfigclient/src/test/java/com/example/serviceconfigclient/ServiceconfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceconfigclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
